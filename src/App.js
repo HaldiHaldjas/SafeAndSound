732b018915ce4acb204d981332bb6c63641aab53
Katrin Aarma <katrin.aarma@hotmail.com>
2023-04-05 15:31:47 +0300

    building login and registration forms 2

--- src/App.js
+++ src/App.js
@@ -1,86 +1,103 @@
+import ReactDOM from "react-dom/client";
 import {useEffect, useState} from "react";
 import './App.css';
-import {Auth} from "./components/auth";
+import { Auth } from "./components/auth";
 import { database } from "./config/firebase";
 import {getDocs, collection, addDoc} from "firebase/firestore"
+import RegistrationForm from "./components/RegistrationForm";
+import { BrowserRouter, Route, Routes, Link, Switch } from "react-router-dom";
 
 
-// my comment
 function App() {
-    const [users, setUsers] = useState([]);
 
-    const [newFirstName, setNewFirstName] = useState("")
-    const [newLastName, setNewLastName] = useState("")
-    const [newEmail, setNewEmail] = useState("")
-    const [newPhone, setNewPhone] = useState(0)
-    const [isNewUserDriver, setIsNewUserDriver] = useState(false)
-
-
-
-
-    const usersCollectionRef = collection(database, "users")
-    const getUsersList = async () => {
-        //read data from firebase, set the users list
-        try {
-            const data = await getDocs(usersCollectionRef)
-            const filteredData = data.docs.map((doc) => ({
-                ...doc.data(),
-                id: doc.id}))
-            console.log(filteredData)
-            setUsers(filteredData)
-        } catch (err) {
-            console.error(err)
-        }
-
-    }
-    useEffect(() => {
-
-        getUsersList()
-    }, [])
-
-    const onSubmitUser = async () => {
-        try {
-        await addDoc(usersCollectionRef, {
-            first_name: newFirstName,
-            last_name: newLastName,
-            email: newEmail,
-            phone: newPhone,
-            driver: isNewUserDriver
-        });
-        getUsersList();
-        } catch (err) {
-            console.error(err)
-        }
-    }
   return (
-    <div className="App">
+      <div className="App">
       <header>
         <h1>Safe and sound</h1>
       </header>
       <main>
-        <div>
-        <Auth></Auth>
-            <div>
-                <input placeholder="First name" onChange={(e) => setNewFirstName(e.target.value)}/>
-                <input placeholder="Last name" onChange={(e) => setNewLastName(e.target.value)}/>
-                <input placeholder="Email" onChange={(e) => setNewEmail(e.target.value)}/>
-                <input placeholder="Phone" type="number"onChange={(e) => setNewPhone(Number(e.target.value))}/>
-                <input type="checkbox" onChange={(e) => setIsNewUserDriver(e.target.checked)}/>
-                <label>I also want to be a driver</label>
-                <button onClick={onSubmitUser}>Registrate</button>
-            </div>
-          <div>
-              {users.map((user) => (
-                  <div>
-                      <p>{user.first_name}</p>
-                      <p>{user.last_name}</p>
-                  </div>
-                  ))}
-          </div>
-        </div>
+          <br/><br/><br/>
+          <BrowserRouter>
+              <div>
+                    <ul>
+                       <Link to="/auth">
+                           <button className="button" style={{width: "100px", padding: "20px"}}>Log in</button>
+                       </Link>
+                    </ul>
+                  <br/>
+                    <ul>
+                      <Link to="/register">
+                          <button className="button" style={{width: "100px", padding: "20px"}}>Register</button>
+                      </Link>
+
+                  </ul>
+
+
+                 <Switch>
+                      <Route path="/auth" element={<Auth />}></Route>
+                      <Route path="/register" element={<RegistrationForm />} />
+
+                      {/* Define additional routes as needed */}
+                 </Switch>
+              </div>
+          </BrowserRouter>
       </main>
     </div>
   );
 }
 
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
 export default App;
+
+
+/*   const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+   const [newFirstName, setNewFirstName] = useState("")
+   const [newLastName, setNewLastName] = useState("")
+   const [newEmail, setNewEmail] = useState("")
+   const [newPhone, setNewPhone] = useState(0)
+   const [isNewUserDriver, setIsNewUserDriver] = useState(false)
+
+
+
+
+   const usersCollectionRef = collection(database, "users")
+
+   const onSubmitUser = async () => {
+       try {
+       await addDoc(usersCollectionRef, {
+           first_name: newFirstName,
+           last_name: newLastName,
+           email: newEmail,
+           phone: newPhone,
+           driver: isNewUserDriver
+       });
+       } catch (err) {
+           console.error(err)
+       }
+   }
+*/
+
+
+      /*    <div>
+              <h3>Sign up</h3>
+              <input placeholder="First name" onChange={(e) => setNewFirstName(e.target.value)}/>
+              <input placeholder="Last name" onChange={(e) => setNewLastName(e.target.value)}/>
+              <input placeholder="Email" onChange={(e) => setNewEmail(e.target.value)}/>
+              <input placeholder="Phone" type="number"onChange={(e) => setNewPhone(Number(e.target.value))}/>
+              <input type="checkbox" onChange={(e) => setIsNewUserDriver(e.target.checked)}/>
+              <label>I also want to be a driver</label>
+              <button onClick={onSubmitUser}>Registrate</button>
+              <a href="Login">I already have an account</a>
+              <div>
+                  {users.map((user) => (
+                      <div>
+                          <p>{user.first_name}</p>
+                          <p>{user.last_name}</p>
+                      </div>
+                  ))}
+              </div>
+          </div>
+          </div>*!/}*/
