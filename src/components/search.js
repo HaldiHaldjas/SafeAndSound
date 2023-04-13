b2b47a5b445d5acf178048958bcfaba8ba9af078
Haldi Ellam <haldi.ellam@hotmail.com>
2023-04-13 09:10:33 +0300

    requests confirmed

---
+++ src/components/search.js
@@ -0,0 +1,14 @@
+let key = 'mama';
+let res;
+
+if (family1[key] === family2[key]) {
+    res = true
+} else {
+    res = false
+}
+console.log(res)
+return (
+    <div>
+        blabla
+    </div>
+)
